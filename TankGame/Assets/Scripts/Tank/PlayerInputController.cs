8eaedef6a2b7d42c04303bc7181f5fdc7d0fe8b1
Leonid Medvedev <leonid_med@mail.ru>
2024-07-17 15:45:07 +0300

    Add transmission settings

--- TankGame/Assets/Scripts/Tank/PlayerInputController.cs
+++ TankGame/Assets/Scripts/Tank/PlayerInputController.cs
@@ -4,52 +4,62 @@ using Zenject;
 
 namespace Tanks
 {
-	/// <summary>
-	/// Обработчик пользовательского ввода
-	/// </summary>
-	public class PlayerInputController : BaseInputController
-	{
-		[Inject]
-		private TankControls.TankActions _tank;
-		[Inject]
-		private TankControls.TurretActions _turret;
+    /// <summary>
+    /// Обработчик пользовательского ввода
+    /// </summary>
+    public class PlayerInputController : BaseInputController
+    {
+        private TankControls _controls;
 
-		[SerializeField, Min(0.1f), Tooltip("Скорость вращения башни")]
-		private float _rotateSpeed = 1f;
-		
-		private void Awake()
-		{
-			_tank.Handbreak.performed += HandbreakChangeEventHandler;
-			_tank.Handbreak.canceled += HandbreakChangeEventHandler;
-			_turret.Fire.performed += FireChangeEventHandler;
-		}
+        private TankControls.TankActions _tank;
 
-		/// <summary>
-		/// Метод обновления считывания ввода
-		/// </summary>
-		/// <remarks>Используется вручную в необходимом цикле обновления</remarks>
-		public override void ManualUpdate()
-		{
-			var direction = _tank.Movement.ReadValue<Vector2>();
-			Acceleration = direction.y;
-			if (direction.x.Equals(0f))
-				TankRotate = TankRotate + (TankRotate > 0f ? -_rotateSpeed : _rotateSpeed) * Time.deltaTime;
-			else
-				TankRotate = TankRotate + direction.x * _rotateSpeed * Time.deltaTime;
-			TankRotate = Mathf.Clamp(TankRotate, -1f, 1f);
-			TurretRotate = _turret.Focus.ReadValue<Vector2>();
-		}
-		
-		private void HandbreakChangeEventHandler(InputAction.CallbackContext obj)
-			=> HandBrake = obj.performed;
-		private void FireChangeEventHandler(InputAction.CallbackContext obj)
-			=> CallFire();
-		
-		private void OnDestroy()
-		{
-			_tank.Handbreak.performed -= HandbreakChangeEventHandler;
-			_tank.Handbreak.canceled -= HandbreakChangeEventHandler;
-			_turret.Fire.performed -= FireChangeEventHandler;
-		}
-	}
+        private TankControls.TurretActions _turret;
+
+        [SerializeField, Min(0.1f), Tooltip("Скорость вращения башни")]
+        private float _rotateSpeed = 1f;
+
+        private void Awake()
+        {
+            _controls = new TankControls();
+
+            _tank = _controls.Tank;
+            _turret = _controls.Turret;
+
+            _tank.Enable();
+            _turret.Enable();
+
+            _tank.Handbreak.performed += HandbreakChangeEventHandler;
+            _tank.Handbreak.canceled += HandbreakChangeEventHandler;
+            _turret.Fire.performed += FireChangeEventHandler;
+        }
+
+        /// <summary>
+        /// Метод обновления считывания ввода
+        /// </summary>
+        /// <remarks>Используется вручную в необходимом цикле обновления</remarks>
+        public override void ManualUpdate()
+        {
+            var direction = _tank.Movement.ReadValue<Vector2>();
+            Acceleration = direction.y;
+            if (direction.x.Equals(0f))
+                TankRotate = TankRotate + (TankRotate > 0f ? -_rotateSpeed : _rotateSpeed) * Time.deltaTime;
+            else
+                TankRotate = TankRotate + direction.x * _rotateSpeed * Time.deltaTime;
+            TankRotate = Mathf.Clamp(TankRotate, -1f, 1f);
+            TurretRotate = _turret.Focus.ReadValue<Vector2>();
+        }
+
+        private void HandbreakChangeEventHandler(InputAction.CallbackContext obj)
+            => HandBrake = obj.performed;
+
+        private void FireChangeEventHandler(InputAction.CallbackContext obj)
+            => CallFire();
+
+        private void OnDestroy()
+        {
+            _tank.Handbreak.performed -= HandbreakChangeEventHandler;
+            _tank.Handbreak.canceled -= HandbreakChangeEventHandler;
+            _turret.Fire.performed -= FireChangeEventHandler;
+        }
+    }
 }
